6d52a4f10fbb9d384aeb498259aaa51e9b36118f
WaltTing <heynew123@163.com>
2017-09-03 00:18:50 +0800

    update

--- 数据结构与算法/char类型转化为int类型.java
+++ 数据结构与算法/char类型转化为int类型.java
@@ -1,8 +1,9 @@
-
-public class zifuchuan {
+public class Shuzhizhuanhuan {
     public static void main(String[] args){
-        String s = "abcd";
-        s = s + "00000000";
-        System.out.println(s);
+        char ch = '1';
+        //将char类型转化为String类型，然后用Integer.parseInt()来转化成十进制
+        int a = Integer.parseInt(String.valueOf(ch));
+        int b = a + 1;
+        System.out.println(b);
     }
 }
