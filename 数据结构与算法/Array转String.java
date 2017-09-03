a99d7f203a27d58ad75419b939e56e19ce8cbe06
WaltTing <heynew123@163.com>
2017-09-03 18:11:15 +0800

    update

---
+++ 数据结构与算法/Array转String.java
@@ -0,0 +1,8 @@
+public class ArraytoString {
+    public static void main(String[] args){
+
+        char ch[] = new char[]{'a','c','t','i','v','e'};
+        String str = new String(ch);
+        System.out.println(str);
+    }
+}
