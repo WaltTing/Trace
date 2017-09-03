1fb15ca79fbd9b8c67cc8a624a3b9155ad5baaea
WaltTing <heynew123@163.com>
2017-09-03 17:06:23 +0800

    update

---
+++ 数据结构与算法/int转String.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class shuzizhuanzifu {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String str = String.valueOf(a);
+        System.out.println(str);
+    }
+}
